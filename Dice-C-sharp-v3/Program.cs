3534d7113c384ab0cab6a1e02446c713c57de6a9
Vladilen Karassev <Vladilen@Karassev.com>
2022-12-29 22:00:26 -0600

    dice in C# v3

--- Dice-C-sharp-v3/Program.cs
+++ Dice-C-sharp-v3/Program.cs
@@ -5,7 +5,6 @@ stopPoint = 100;
 var currentPointsSum = new Int32();
 Random rnd = new Random();
 int dice = rnd.Next(1, 7);
-int[] answers = new int[6];
 List<int> results = new List<int>();
 
 for (int i = 0; i < setsLimit; i++)
@@ -21,13 +20,14 @@ for (int i = 0; i < setsLimit; i++)
     results.Add(currentPointsSum);
 }
 
-// var groupedResults = results.GroupBy(r => r);
-results.Sort();
-results.GroupBy(r => r);
-foreach (var item in results)
+var answers = results.GroupBy(x => x)
+                           .Select(grp => new { number = grp.Key, count = grp.Count() })
+                           .ToArray();
+
+foreach (var item in answers)
 {
     Console.WriteLine("Results: {0}", item);
 }
 
-Console.WriteLine("Results count: {0}", results.Count);
+Console.WriteLine("Results count: {0}", answers.Length);
 
